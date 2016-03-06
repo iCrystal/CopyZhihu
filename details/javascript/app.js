59dd36e206e6f75d744b37b91d5190c655959302
e <1367687435@qq.com>
2016-03-07 07:03:08 +0800

    second commit

--- details/javascript/app.js
+++ details/javascript/app.js
@@ -5,27 +5,25 @@ window.onload=function(){
 	var Obtn4=document.querySelector('#btn4');
 	var Obtn5=document.querySelector('#btn5');
 	var Obtn6=document.querySelector('#btn6');
+	var Otextdec=document.querySelector('#textdec');
 	Obtn1.addEventListener('click',function(){
-		if(btn1.style.backgroundColor='#f2f2f2'){
-			btn1.style.backgroundColor="#a8a8cb";
-			btn1.style.border="1px solid #a9a9a9";
-			btn1.style.borderRadius="3px";
-		}else{
-			btn1.style.backgroundColor="#f2f2f2";
-			btn1.style.border="none";
-			btn1.style.borderRadius="none";
-		}
-		
+		btn1.style.backgroundColor="#a8a8cb";
+		btn1.style.border="1px solid #a9a9a9";
+		btn1.style.borderRadius="3px";
+		Otextdec.style.fontWeight="bold";
+
 	});
 	Obtn2.addEventListener('click',function(){
 		btn2.style.backgroundColor="#a8a8cb";
 		btn2.style.border="1px solid #a9a9a9";
 		btn2.style.borderRadius="3px";
+		Otextdec.style.fontStyle="italic";
 	});
 	Obtn3.addEventListener('click',function(){
 		btn3.style.backgroundColor="#a8a8cb";
 		btn3.style.border="1px solid #a9a9a9";
 		btn3.style.borderRadius="3px";
+		Otextdec.style.textDecoration="underline"
 	});
 	Obtn4.addEventListener('click',function(){
 		btn4.style.backgroundColor="#a8a8cb";
@@ -43,6 +41,4 @@ window.onload=function(){
 		btn6.style.borderRadius="3px";
 	});
 	
-	
-
 }
